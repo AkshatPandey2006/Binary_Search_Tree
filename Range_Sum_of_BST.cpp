a543e57872f86ef2d0476de95c7581e4d248be4f
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-06-24 20:55:51 +0530

    Create Range_Sum_of_BST.cpp

---
+++ Range_Sum_of_BST.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int inorder(TreeNode *root, int left, int right) {
+        if(root) {
+            inorder(root -> left, left, right);
+
+            if(root -> val >= left and root -> val <= right) {
+                sum += root -> val;
+            } 
+
+            inorder(root -> right, left, right);
+        }
+        return sum;
+    }
+
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        return inorder(root, low, right);
+    }
+};
