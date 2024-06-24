ca26c7fec21f9c40b1449e0f13d83f59d0884942
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-06-24 23:15:37 +0530

    Create Find_Mode_in_BST.cpp

---
+++ Find_Mode_in_BST.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int val = INT_MIN;
+    int freq = 0;
+    int maxi = 0;
+    vector<int> ans;
+
+    void inorder(TreeNode *root) {
+        if(root == NULL) return;
+        inorder(root -> left);
+
+        if(val == root -> val) {
+            freq++;
+        }
+        else {
+            val = root -> val;
+            freq = 1;
+        }
+        if(freq > maxi) {
+            maxi = freq;
+            ans = {root -> val};
+        }
+        else if(freq == maxi) {
+            ans.push_back(root -> val);
+        }
+
+        inorder(root -> right);
+    } 
+
+public:
+    vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return ans;
+    }
+};
